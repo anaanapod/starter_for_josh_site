1049971dd19a08c0548691d018039541f95b1fe1
anaanapod <79947261+anaanapod@users.noreply.github.com>
2021-03-18 11:32:32 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="container-fluid">
+    <div class= "container">
+          <div class="row">
+        <div class="col-md-6 text-center footer-content">
+            <p><?php bloginfo('name'); ?></p>
+            
+            <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="facebook">
+            <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="phone">
+            
+              </div>
+              
+              <div class="col-md-6 text=center footer-content">
+                  <p>Terms and Conditions</p>
+                  
+              <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="in">   
+                  <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg"  alt="twitter">
+              </div>
+        
+        
+        </div>
+<!--        row-->
+          </div> 
+<!--          container -->
+      </footer>
+      
+    
+
+
+  </body>
+</html>
+<?php wp_footer(); ?>
