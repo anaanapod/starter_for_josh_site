37a40e864808d1a4a774c238995be76c8adf6060
anaanapod <79947261+anaanapod@users.noreply.github.com>
2021-03-18 11:32:45 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+ <?php get_header(); /* Tells WordPress to include header.php */ ?>
+
+      
+      <section class="container-fluid aboutbg text-center">
+        
+          <div class="container">
+          
+          <h2> GAMES &amp; LEVEL DESIGNER </h2>
+          <p class="particle"> and practical wizard</p>
+              <p class="about-text"> My name is Josh Whitkin. I'm a professional with more than 7 years industry experience in Games Design, Level Design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create and interactive experince. Whether I'm designing for puzzle games, 2D platforms or 3D environments. I always tell a story.</p> 
+          
+              <div class="blocker"></div>
+          </div>
+          
+      
+      </section>
+      
+      <section class="container-fluid articlesbg">
+          <div class="container">
+          <div class="row">
+            <div>
+              <h3 class="text-center article-title">LATEST ARTICLES</h3>
+              </div>  
+              
+              <div class="col-md-4 article-cent" >
+                  <img class="img-responsive" alt="imageone" src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/imageone.jpg"> 
+                  
+                  <h4> A PORTRAIT</h4>
+                  <p class="date"> Friday, November 10.2019 </p>
+                  <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
+                  </p>
+                  <p class="readmore"> CONTINUE READING</p>
+              </div>
+              
+              
+             <div class="col-md-4 article-cent">
+                 
+                 <img class="img-responsive" alt="imageone" src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/image2.jpg"> 
+                 
+                  <h4> GAME DESIGN</h4>
+                  <p class="date"> Friday, November 10.2019 </p>
+                  <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
+                  </p>
+                  <p class="readmore"> CONTINUE READING</p>
+              </div>
+              
+              
+              
+              <div class="col-md-4 article-cent">
+                  
+                  <img class="img-responsive" alt="imageone" src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/image3.jpg"> 
+                  
+                  <h4> A MODEL</h4>
+                  <p class="date"> Friday, November 10.2019 </p>
+                  <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. 
+                  </p>
+                  <p class="readmore"> CONTINUE READING</p>
+              </div>
+              
+              
+              
+              
+              
+              </div>  
+<!--           row-->
+              </div>
+<!--          container-->
+          </section>
+      <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
