d630caa70c8cc5eb07e0b3a3e5666577de1014a9
anaanapod <79947261+anaanapod@users.noreply.github.com>
2021-03-18 15:05:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
- <?php get_header(); /* Tells WordPress to include header.php */ ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
 
       
       <section class="container-fluid aboutbg text-center">
         
           <div class="container">
           
-          <h2> GAMES &amp; LEVEL DESIGNER </h2>
-          <p class="particle"> and practical wizard</p>
-              <p class="about-text"> My name is Josh Whitkin. I'm a professional with more than 7 years industry experience in Games Design, Level Design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create and interactive experince. Whether I'm designing for puzzle games, 2D platforms or 3D environments. I always tell a story.</p> 
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=2');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>    
+
           
               <div class="blocker"></div>
           </div>
@@ -66,4 +77,4 @@
               </div>
 <!--          container-->
           </section>
-      <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
