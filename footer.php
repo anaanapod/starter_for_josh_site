22a5f9080a15f5e865e4e26a33c2156fc33c170b
anaanapod <79947261+anaanapod@users.noreply.github.com>
2021-03-18 13:54:38 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
         <div class="col-md-6 text-center footer-content">
             <p><?php bloginfo('name'); ?></p>
             
-            <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="facebook">
-            <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="phone">
+            <img src="http://206.189.45.97/~mesh20/sub/wp-content/themes/starter_for_josh_site/images%20/facebook.jpg" alt="facebook">
+            <img src="http://206.189.45.97/~mesh20/sub/wp-content/themes/starter_for_josh_site/images%20/phone.jpg" alt="phone">
             
               </div>
               
@@ -13,7 +13,7 @@
                   <p>Terms and Conditions</p>
                   
               <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg" alt="in">   
-                  <img src="https://github.com/anaanapod/starter_for_josh_site/blob/master/images%20/in.jpg"  alt="twitter">
+                  <img src="http://206.189.45.97/~mesh20/sub/wp-content/themes/starter_for_josh_site/images%20/twitter.jpg"  alt="twitter">
               </div>
         
         
